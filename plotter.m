1a785ebdba819f0bf3d4e36b1f3197665398c0da
Diogo <da.filipe@campus.fct.unl.pt>
2025-09-18 20:01:48 +0100

    go

---
+++ plotter.m
@@ -0,0 +1,56 @@
+tic
+
+
+
+% main_script.m
+
+% Definições
+Hoosier = 'Hoosier 20.5X7.0-13, R20';
+Goodyear ='Goodyear 20.0X7.0-13';
+Hoosier_18 = 'Hoosier 18.0X6.0-10';
+Goodyear_18 ='Goodyear 18.0X6.5-10, Eagle Racing Special';
+MRF ='MRF 18.0X6.0-10, ZTD1';
+tol = 100;
+rimWidth = 7.0;
+minfich = 16;
+maxfich = 79;
+choice = 0;    
+testChoice = 0; % 1 = Cornering, 2 = Drive/Brake
+folderPath = 'RawData_Cornering_ASCII_SI_Round9_Runs16to49';
+
+fprintf("Escolhe o tipo de pneu para estudo:")
+fprintf(" Hoosier 20.5X7.0-13, R20 :1\n Goodyear 20.0X7.0-13:2\n Hoosier 18.0X6.0-10:3\n Goodyear 18.0X6.5-10, Eagle Racing Special:4\n MRF 18.0X6.0-10, ZTD1:5\n");
+choice = input('');
+switch tire_choice
+    case 1, tireName = Hoosier;
+    case 2, tireName = Goodyear;
+    case 3, tireName = Hoosier_18;
+    case 4, tireName = Goodyear_18;
+    case 5, tireName = MRF;
+    otherwise, error('Opção inválida.');
+end
+
+clc
+fprintf ("Escolhe que tipo de testes queres recolher \n Cornering-1 \n Drive Brake-2\n");
+testChoice = input('');
+
+    if testChoice == 1
+        folderPath = 'RawData_Cornering_ASCII_SI_Round9_Runs16to49';
+        fprintf("Cornering selecionado\n");
+    elseif testChoice == 2  
+        folderPath = 'RawData_DriveBrake_ASCII_SI_Round9';
+        fprintf("DriveBrake selecionado\n");
+    else
+        error('Opção inválida. Escolhe 1 (cornering) ou 2 (drivebrake).');
+    end
+
+
+% Chamada da função
+[SA, FZ, FY, MZ] = read_calspan_data(folderPath,tireName, rimWidth, minfich, maxfich);
+
+
+
+
+% Exemplo de visualização
+
+toc
--- read_calspan_data.m
+++ read_calspan_data.m
@@ -1,75 +1,60 @@
-function [SA, FZ, FY, MZ] = read_calspan_data(tireName, rimWidth, minfich, maxfich)
+function [SA, FZ, FY, MZ] = read_calspan_data(folderPath, tireName, rimWidth, minfich, maxfich)
 
-    % inicializar cell arrays para acumular dados
-    SA_cell = {};
-    FZ_cell = {};
-    FY_cell = {};
-    MZ_cell = {};
+    SA = [];
+    FZ = [];
+    FY = [];
+    MZ = [];
 
-
-    % escolher tipo de teste
-    fprintf("Escolhe que tipo de testes queres recolher \n Cornering-1 \n Drive Brake-2\n");
-    choice = input('');
-
-    if choice == 1
-        folderPath = 'RawData_Cornering_ASCII_SI_Round9_Runs16to49';
-        fprintf("Cornering selecionado\n");
-    elseif choice == 2  
-        folderPath = 'RawData_DriveBrake_ASCII_SI_Round9';
-        fprintf("DriveBrake selecionado\n");
-    else
-        error('Opção inválida. Escolhe 1 (cornering) ou 2 (drivebrake).');
-    end
-
-    % loop pelos ficheiros
+    % Loop pelos ficheiros
     for i = minfich:maxfich
         filename = sprintf('B2356raw%d.dat', i);
         filepath = fullfile(folderPath, filename);
 
-        if exist(filepath, 'file')
-            fid = fopen(filepath, 'r');
-            if fid == -1
-                warning("Não foi possível abrir o ficheiro: %s", filename);
-                continue;
-            end
-
-            % ler primeira linha do header
-            headerLine = fgetl(fid);
-
-            % verificar pneu e jante
-            tireCheck = contains(headerLine, tireName, 'IgnoreCase', true); 
-            rimCheck  = contains(headerLine, sprintf('Rim_Width=%g', rimWidth));
-
-            if ~tireCheck || ~rimCheck
-                fprintf("Ficheiro %s ignorado (pneu ou jante não correspondem)\n", filename);
-                fclose(fid);
-                continue;
-            end
-
-            % saltar as próximas 2 linhas do header
-            fgetl(fid); 
-            fgetl(fid);
-
-            % ler dados numéricos
-            data = textscan(fid, repmat('%f',1,21), 'Delimiter', '\t');
-
-            % adicionar aos cell arrays
-            SA_cell{end+1} = data{4};
-            FZ_cell{end+1} = data{11};
-            FY_cell{end+1} = data{10};
-            MZ_cell{end+1} = data{13};
-
-
-            fclose(fid);
-
-        else
-            fprintf("Ficheiro não encontrado: %s\n", filename);
+        if ~isfile(filepath)
+            continue;
         end
+
+        fid = fopen(filepath, 'r');
+        if fid < 0, continue; end
+
+        % Verificar pneu e jante
+        headerLine = fgetl(fid);
+        if ~contains(headerLine, tireName, 'IgnoreCase', true) || ...
+           ~contains(headerLine, sprintf('Rim_Width=%g', rimWidth))
+            fclose(fid);
+            continue;
+        end
+
+        % Ler nomes das colunas e ignorar linha das unidades
+        colNamesLine = fgetl(fid);
+        fgetl(fid); % unidades
+
+        colNames = strsplit(strtrim(colNamesLine), '\t');
+        numCols = numel(colNames);
+
+        % Índices das colunas desejadas
+        idx_SA = find(strcmpi(colNames, 'SA'));
+        idx_FZ = find(strcmpi(colNames, 'FZ'));
+        idx_FY = find(strcmpi(colNames, 'FY'));
+        idx_MZ = find(strcmpi(colNames, 'MZ'));
+
+        if any([isempty(idx_SA), isempty(idx_FZ), isempty(idx_FY), isempty(idx_MZ)])
+            fclose(fid);
+            continue;
+        end
+
+        % Lê todos os dados como números
+        format = repmat('%f', 1, numel(colNames));
+        data = textscan(fid, format, 'Delimiter', '\t');
+        fclose(fid);
+
+        SA = [SA; data{idx_SA}];
+        FZ = [FZ; data{idx_FZ}];
+        FY = [FY; data{idx_FY}];
+        MZ = [MZ; data{idx_MZ}];
     end
 
-    % concatenar todos os vetores no final
-    SA = vertcat(SA_cell{:});
-    FZ = vertcat(FZ_cell{:});
-    FY = vertcat(FY_cell{:});
-    MZ=  vertcat(MZ_cell{:});
+    if isempty(SA)
+        error("Nenhum ficheiro válido foi lido.");
+    end
 end
